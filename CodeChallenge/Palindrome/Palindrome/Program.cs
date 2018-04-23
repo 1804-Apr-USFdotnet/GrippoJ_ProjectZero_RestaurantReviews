5745925fc589317c25507ed60fc0b1dc13f712d7
James Grippo <jgrippo95@gmail.com>
2018-04-23 11:15:28 -0400

    Code Challenge
    Implemented Palindrome Class

---
+++ CodeChallenge/Palindrome/Palindrome/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputStr = "Race car";
+            Stripper test = new Stripper();
+            String outputStr = test.StripNonAlphaNumeric(inputStr.Replace(" ",""));
+            Console.WriteLine(outputStr);
+            Palindrome pMoney = new Palindrome();
+            pMoney.printer(outputStr);
+            Console.Read();
+        }
+    }
+
+    class Stripper
+    {
+        public string StripNonAlphaNumeric(string inputStr)
+        {
+            Regex rgx = new Regex("[^a-zA-Z0-9 -]");
+            inputStr = rgx.Replace(inputStr, "");
+            return inputStr;
+        }
+    }
+
+    class Palindrome
+    {
+        public bool checkPalindrome(String inputStr)
+        {
+            string notCaseSensitive = inputStr.ToLower();
+            char[] charArray = notCaseSensitive.ToCharArray();
+            Array.Reverse(charArray);
+            string reversedString = new String(charArray);
+            return (notCaseSensitive == reversedString);
+
+        }
+
+        public void printer(String inputStr)
+        {
+            if (this.checkPalindrome(inputStr))
+            {
+                Console.WriteLine("This is a Palindrome,\n");
+            }
+            else
+            {
+                Console.WriteLine("This is not a Palindrome.\n");
+            }
+        }
+    }
+}
