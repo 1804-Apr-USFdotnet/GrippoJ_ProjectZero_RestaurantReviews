cbf1cd3037af6a49b469b5d4e19d07f0f93c5aff
James Grippo <jgrippo95@gmail.com>
2018-04-24 10:08:54 -0400

    Review model implementation

--- ProjectZero/RestaurantReviews/PZModels/Review.cs
+++ ProjectZero/RestaurantReviews/PZModels/Review.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace PZModels
 {
-    class Class2
+    class Review
     {
+        public Guid Id { get; set; }
+        public string RestaurantKey { get; set; }
+        public string Reviewer { get; set; }
+        public string Description { get; set; }
+        public int Rating { get; set; }
+
+        public override string ToString()
+        {
+            return $"\nReviewer: {Reviewer}\nReview: {Description}\nRating: {Rating}\n";
+        }
     }
 }
