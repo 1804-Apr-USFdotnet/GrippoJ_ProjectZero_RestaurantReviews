8ab1a4cc54e14d768628509b9d3e1e84e4557706
James Grippo <jgrippo95@gmail.com>
2018-04-24 09:55:58 -0400

    Forgot to save models file

--- ProjectZero/RestaurantReviews/PZModels/Restaurant.cs
+++ ProjectZero/RestaurantReviews/PZModels/Restaurant.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace PZModels
 {
-    public class Class1
+    public class Restaurant
     {
+        public int rIndex { get; set; }
+        public string key { get; set; }
+        public string name { get; set; }
+        public string city { get; set; }
+        public string zipcode { get; set; }
+        public string state { get; set; }
+        public string address { get; set; }
+        public double avgRating  { get; set; }
+
+        public override string ToString()
+        {
+            return
+                $"\nindex: {rIndex}\nname: {name} \ncity: {city}\nZip: {zipcode}\nstate: {state}\naddress: {address}\naverage rating: {avgRating}\n";
+        }
+
+        public void calcAvgRating(IEnumerable<Review> reviews)
+        {
+            avgRating = reviews.Select(x => x.Rating).Average();    
+        }
     }
 }
