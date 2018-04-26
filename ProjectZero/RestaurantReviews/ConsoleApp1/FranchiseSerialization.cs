b459351295599a0912d52bb5db90f850b88d9311
James Grippo <jgrippo95@gmail.com>
2018-04-26 15:33:25 -0400

    Finished Franchise serialization

---
+++ ProjectZero/RestaurantReviews/ConsoleApp1/FranchiseSerialization.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using PZModels;
+
+namespace ConsoleApp1
+{
+    class FranchiseSerialization
+    {
+        public List<Franchise> GetFranchisesXML()
+        {
+            FranchiseCollection f = null;
+            string path = "Franchise.xml";
+
+            XmlSerializer serializer = new XmlSerializer(typeof(FranchiseCollection));
+
+            using (StreamReader reader = new StreamReader(path))
+                f = (FranchiseCollection)serializer.Deserialize(reader);
+            return f.FranchiseCollectionList;
+
+        }
+    }
+
+    [XmlRoot("FranchiseCollection")]
+    public class FranchiseCollection
+    {
+        [XmlArray("Franchises")]
+        [XmlArrayItem("Franchise", typeof(Franchise))]
+        public List<Franchise> FranchiseCollectionList { get; set; }
+    }
+}
