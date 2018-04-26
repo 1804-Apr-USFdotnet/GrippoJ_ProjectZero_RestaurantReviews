97dfb79158440382f3331161acfb01ca6192a440
James Grippo <jgrippo95@gmail.com>
2018-04-26 11:51:19 -0400

    added the franchise repo

---
+++ ProjectZero/RestaurantReviews/PZRepositories/FranchiseRepo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PZModels;
+using PZRepositoryInterface;
+
+namespace PZRepositories
+{
+    class FranchiseRepo : IFranchiseRepo
+    {
+        private readonly PZRepoContext _pzRepoContext;
+
+        public FranchiseRepo(PZRepoContext context)
+        {
+            _pzRepoContext = context;
+        }
+        public Franchise GetById()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Franchise> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Franchise> GetByGenre()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(Franchise franchise)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateFranchises()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ProjectZero/RestaurantReviews/PZRepositoryInterface/IFranchiseRepo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PZModels;
+
+namespace PZRepositoryInterface
+{
+    public interface IFranchiseRepo
+    {
+        Franchise GetById();
+        IEnumerable<Franchise> GetAll();
+        IEnumerable<Franchise> GetByGenre();
+        void Add(Franchise franchise);
+        void UpdateFranchises();
+    }
+}
