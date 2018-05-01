584e228997218d6b99be2b0413b2afac9a1e070b
James Grippo <jgrippo95@gmail.com>
2018-05-01 14:07:14 -0400

    WIP

--- ProjectZero/RestaurantReviews/DataTests/RestaurantRepoTest.cs
+++ ProjectZero/RestaurantReviews/DataTests/RestaurantRepoTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data.Entity;
 using IPZServices;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -16,6 +17,7 @@ namespace DataTests
         private readonly Mock<IPZRepoContext> _moqContext;
         private readonly Mock<DbSet<Restaurant>> _moqSet;
         private readonly Restaurant r;
+        private readonly List<Restaurant> restaurants;
 
         public RestaurantRepoTest()
         {
@@ -24,17 +26,58 @@ namespace DataTests
 
             _moqContext.Setup(m => m.Restaurants).Returns(_moqSet.Object);
 
-            r = new Restaurant
+            #region restaurants
+
+            restaurants = new List<Restaurant>()
             {
-                rIndex = 1,
-                FranchiseID = 1,
-                Name = "TestRestaurant",
-                City = "city",
-                Zipcode = "10801",
-                State = "NY",
-                Address = " 1 a",
-                AvgRating = 6.9
+                new Restaurant
+                {
+                    rIndex = 1,
+                    FranchiseID = 1,
+                    Name = "ZestRestaurant",
+                    City = "Zcity",
+                    Zipcode = "90801",
+                    State = "ZNY",
+                    Address = "Z 1 a",
+                    AvgRating = 6.9
+                },
+                new Restaurant
+                {
+                    rIndex = 2,
+                    FranchiseID = 2,
+                    Name = "TestRestaurant2",
+                    City = "city2",
+                    Zipcode = "10802",
+                    State = "NY",
+                    Address = " 2 a",
+                    AvgRating = 7.9
+                },
+                new Restaurant
+                {
+                    rIndex = 3,
+                    FranchiseID = 3,
+                    Name = "TestRestaurant3",
+                    City = "city3",
+                    Zipcode = "10803",
+                    State = "NY",
+                    Address = " 3 a",
+                    AvgRating = 8.9
+                },
+                new Restaurant
+                {
+                    rIndex = 4,
+                    FranchiseID = 4,
+                    Name = "TestRestaurant4",
+                    City = "city4",
+                    Zipcode = "10804",
+                    State = "NY",
+                    Address = " 4 a",
+                    AvgRating = 9.9
+                }
             };
+
+            #endregion
+            _moqContext.Setup()
         }
 
         [TestMethod]
@@ -53,6 +96,33 @@ namespace DataTests
             service.Add(r);
 
             _moqContext.Verify(m => m.SaveChanges(), Times.Once);
+        }
+
+        [TestMethod]
+        public void GetById_PassedInt_AssertReturnType()
+        {
+            var service = new RestaurantRepo(_moqContext.Object);
+            var restById = service.GetById(It.IsAny<int>());
+
+            //Assert.IsInstanceOfType(restById, typeof(Restaurant));
+        }
+
+        [TestMethod]
+        public void GetByName_PassedString_AssertReturnType()
+        {
+            var service = new RestaurantRepo(_moqContext.Object);
+            var restByName = service.GetByName(It.IsAny<string>());
+
+            //Assert.IsInstanceOfType(restByName, typeof(Restaurant));
+        }
+
+        [TestMethod]
+        public void GetAll_AssertReturnType()
+        {
+            var service = new RestaurantRepo(_moqContext.Object);
+            var restById = service.GetAll();
+
+            Assert.IsInstanceOfType(restById, typeof(IEnumerable<Restaurant>));
         }
     }
 }
